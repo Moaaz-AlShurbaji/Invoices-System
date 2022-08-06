ef72ae41f2f898b277319219ecfb67817a248c18
unknown <symouaz@gmail.com>
2022-08-06 14:51:29 +0300

    invoice details and invoice attachment migrations, models and controllers

---
+++ app/Http/Controllers/InvoiceAttachmentsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice_Attachments;
+use Illuminate\Http\Request;
+
+class InvoiceAttachmentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice_Attachments  $invoice_Attachments
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoice_Attachments $invoice_Attachments)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invoice_Attachments  $invoice_Attachments
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invoice_Attachments $invoice_Attachments)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Invoice_Attachments  $invoice_Attachments
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invoice_Attachments $invoice_Attachments)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoice_Attachments  $invoice_Attachments
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice_Attachments $invoice_Attachments)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InvoiceDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Invoice_Detail;
+use Illuminate\Http\Request;
+
+class InvoiceDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Invoice_Detail  $invoice_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Invoice_Detail $invoice_Detail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Invoice_Detail  $invoice_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Invoice_Detail $invoice_Detail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Invoice_Detail  $invoice_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Invoice_Detail $invoice_Detail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Invoice_Detail  $invoice_Detail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Invoice_Detail $invoice_Detail)
+    {
+        //
+    }
+}
